9d8cf8866f283d88598b9aa0ae42289145a44170
Andrea Lopez <andrealoga33@gmail.com>
2023-12-05 11:52:16 +0100

    Add Http Error in order to create repo and auth

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -13,20 +13,23 @@ export type TokenPayload = {
 
 export abstract class Auth {
   static secret = process.env.JWT_SECRET;
-  static hash(value:string):Promise<string>{
-   const saltRound = 10 
-  return hash(value,saltRound) 
-  }
-  
-  static compare(value:string, hash:string): Promise<boolean> {
-    return jwt.sign(payload, Auth.secret!)
+  static hash(value: string): Promise<string> {
+    const saltRound = 10;
+    return hash(value, saltRound);
   }
 
-  static verifyAndGetPayload(token:string){
+  static compare(value: string, hash: string): Promise<boolean> {
+    return jwt.sign(payload, Auth.secret!);
+  }
+
+  static verifyAndGetPayload(token: string) {
     try {
-      const result = jwt.verify(token, Auth.secret!)
-    } catch () {
-      
+      const result = jwt.verify(token, Auth.secret!);
+      if (typeof result === 'string')
+        throw new HttpError(498, 'Invalid token', result);
+      return result as TokenPayload;
+    } catch (error) {
+      throw new HttpError(498, 'Invalid token', (error as Error).message);
     }
   }
 }
