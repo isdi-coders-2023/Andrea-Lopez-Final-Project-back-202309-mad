45282563c0f52b3dc20a649fb0da490f9256fb8d
Andrea Lopez <andrealoga33@gmail.com>
2023-12-05 17:36:07 +0100

    Create image data models and media files in order to use cloudinary

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from 'express';
+import createDebug from 'debug';
+import { Auth } from '../services/auth.js';
+import { User } from '../entities/user.js';
+import { Controller } from './controller.js';
+import { LoginResponse } from '../types/login.response.js';
+import { HttpError } from '../types/http.error.js';
+import { UsersMongoRepo } from '../repo/users/users.mongo.repo.js';
+
+const debug = createDebug('PF:users:controller');
+
+export class UsersController extends Controller<User> {
+  constructor(protected repo: UsersMongoRepo) {
+    super(repo);
+    debug('Instantiated');
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = req.body.userId
+        ? await this.repo.getById(req.body.userId)
+        : await this.repo.login(req.body);
+
+      const data: LoginResponse = {
+        user: result,
+        token: Auth.signJWT({
+          id: result.id,
+          email: result.email,
+        }),
+      };
+      res.status(202);
+      res.statusMessage = 'Accepted';
+      res.json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.file)
+        throw new HttpError(406, 'Not Acceptable', 'Invalid multer file');
+      const imgData = await this.cloudinaryService.uploadImage(req.file.path);
+      req.body.avatar = imgData;
+      super.create(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
